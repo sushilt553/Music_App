ee23195f3aafedfe1c147490bf259c6d780675ba
Sushil <sushilt553@gmail.com>
2020-06-26 17:14:16 -0400

    Band index and show done

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -1,12 +1,23 @@
 class BandsController < ApplicationController
 
     def index
+        @bands = Band.all
+        render :index
     end
 
     def show
+        @band = Band.find_by(id: params[:id])
+
+        if @band
+            redirect_to band_url(@band)
+        else
+            flash[:errors] = ["Band not found"]
+            redirect_to bands_url
+        end
     end
 
     def new
+        render :new
     end
 
     def create
@@ -19,4 +30,5 @@ class BandsController < ApplicationController
     end
 
     def destroy
+    end
 end
