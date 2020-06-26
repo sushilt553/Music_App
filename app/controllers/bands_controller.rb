3b8632691507398213c83ce4a1a6c4a5837e6c04
Sushil <sushilt553@gmail.com>
2020-06-26 17:16:59 -0400

    Create a band

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -21,6 +21,14 @@ class BandsController < ApplicationController
     end
 
     def create
+        @band = Band.new(band_params)
+
+        if @band.save
+            redirect_to band_url(@band)
+        else
+            flash.now[:errors] = @band.errors.full_messages
+            render :new
+        end
     end
 
     def edit
