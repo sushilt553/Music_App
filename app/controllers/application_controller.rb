cda88f11fd57f0e6f4ca3d454c75eae2df23a50c
Sushil <sushilt553@gmail.com>
2020-06-26 16:50:47 -0400

    Sessions controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+
+    helper_method :current_user, :logged_in?
+
+    def current_user
+    end
+
+    def logged_in?
+    end
+
+    def log_in_user(user)
+    end
+
+    def require_login
+    end
+
+    def log_out_user
+    end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,17 @@ class SessionsController < ApplicationController
     end
     
     def create
+        user = User.find_by_credentials(
+            params[:user][:username],
+            params[:user][:password]
+        )
+
+        if user
+            
+        else
+            flash[:errors] = ["Username/Password didn't match"]
+            render :new
+        end
     end
 
     def destroy
