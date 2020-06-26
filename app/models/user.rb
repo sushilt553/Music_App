7f3758fb171a7e95d6f74ed18511afc3bc9912ae
Sushil <sushilt553@gmail.com>
2020-06-26 16:16:10 -0400

    FGRIPE ready

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,21 @@ class User < ApplicationRecord
     validates :password_digest, presence: true
     validates :password, length: {minimum: 6, allow_nil: true}
 
+    def self.find_by_credentials(username, password) 
+    end
     
+    def self.generate_session_token
+    end
+
+    def reset_session_token!
+    end
+
+    def is_password?(password) 
+    end
+
+    def password=(password) 
+    end
+
+    def ensure_session_token
+    end
 end
