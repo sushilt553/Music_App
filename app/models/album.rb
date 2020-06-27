144d0f2a830494b72618c654a55b1c68719e2034
Sushil <sushilt553@gmail.com>
2020-06-27 12:04:36 -0400

    Association and album model complete

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,2 +1,10 @@
 class Album < ApplicationRecord
+
+    validates :title, :year, :live, presence: true
+    validates :live, inclusion: {in: [true, false]}
+
+    belongs_to :band,
+        primary_key: :id,
+        foreign_key: :band_id,
+        class_name: :Band
 end
