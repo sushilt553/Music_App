f1e7d05b4c2906a418d202a3c9ece6e0916ee2de
Sushil <sushilt553@gmail.com>
2020-06-28 15:51:28 -0400

    Create controller for tracks complete

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -11,12 +11,21 @@ class TracksController < ApplicationController
         if @track.save
             redirect_to track_url(@track)
         else
+            @album = @track.album
             flash.now[:errors] = @track.errors.full_messages
             render :new
         end
     end
 
     def show
+        @track = Track.find_by(id: params[:id])
+
+        if @track
+            render :show
+        else
+            flash[:errors] = ["Track not found"]
+            redirect_to root_url
+        end
     end
 
     def edit
