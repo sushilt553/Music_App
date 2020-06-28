b8def7e8e4b807f02b14192ac28ae79b62e5366b
Sushil <sushilt553@gmail.com>
2020-06-28 16:16:38 -0400

    before action for all bands

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
         user = User.new(user_params)
 
         if user.save
+            log_in_user(user)
         else
             flash.now[:errors] = user.errors.full_messages
             render :new
