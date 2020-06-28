c80ca300799a3d46789fb243fcb81134986d273c
Sushil <sushilt553@gmail.com>
2020-06-28 15:32:30 -0400

    Create action in tracks controller

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -6,6 +6,17 @@ class TracksController < ApplicationController
     end
 
     def create
+        @track = Track.new(track_params)
+
+        if @track.save
+            redirect_to track_url(@track)
+        else
+            flash.now[:errors] = @track.errors.full_messages
+            render :new
+        end
+    end
+
+    def show
     end
 
     def edit
